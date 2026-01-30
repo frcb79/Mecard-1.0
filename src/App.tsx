c23e520f30be55089aa375ea585381329b1b208f
frcb79 <francocb79@gmail.com>
2026-01-29 22:29:58 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AdminLayout from "./components/AdminLayout";
-import Schools from "./views/Schools";
+import SuperAdminDashboard from "./components/SuperAdminDashboard";
+import Schools from "./views/schools";
+import Students from "./views/Students";
 import SchoolDetail from "./views/SchoolDetail";
 
 export default function App() {
   return (
-    <Routes>
-      <Route element={<AdminLayout />}>
-        <Route path="/" element={<Navigate to="/schools" replace />} />
-        <Route path="/schools" element={<Schools />} />
-        <Route path="/schools/:id" element={<SchoolDetail />} />
-      </Route>
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AdminLayout />}>
+          <Route path="/" element={<SuperAdminDashboard />} />
+          <Route path="/schools" element={<Schools />} />
+          <Route path="/schools/:id" element={<SchoolDetail />} />
+          <Route path="/students" element={<Students />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
